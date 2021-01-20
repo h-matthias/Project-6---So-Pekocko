15d58eed4043c935b953bb09ff88283d2abd225d
Hoarau Matthias <h.matthias@live.fr>
2021-01-20 09:22:25 +0400

    modif schema User

--- models/User.js
+++ models/User.js
@@ -2,8 +2,7 @@ const mongoose = require ("mongoose");
 const uniqueValidator = require("mongoose-unique-validator");
 
 const userSchema = mongoose.Schema({
-    userId: {type: String, required: true },
-    email: {type: String, unique: true, required: true, uniqueCaseInsensitive: true },
+    email: {type: String, unique: true, required: true },
     password: {type: String, required: true},
 });
 

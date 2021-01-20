b14d664ff6b062cdf9d69153f9c681bfd0c40084
Hoarau Matthias <h.matthias@live.fr>
2021-01-20 11:09:00 +0400

    middleware d'auth

---
+++ middleware/auth.js
@@ -0,0 +1,16 @@
+const jwt = require("jsonwebtoken");
+
+module.exports = (req, res, next) =>{
+    try{
+        const token = req.headers.authorization.split(" ")[1];
+        const decodetToken = jwt.verify(token, process.env.JWT_PASS);
+        const userId = decodetToken.userId;
+        if (req.body.userId && req.body.userId !==userId){
+            throw "Utilisateur non valable";
+        } else {
+            next();
+        }
+    } catch (error) {
+        res.status(401).json({ error: error | "Requête non autentifiée" });
+    }
+};

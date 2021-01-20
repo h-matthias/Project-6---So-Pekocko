01c05ba61a231e2edb8ceec6b0b110dd02dad436
Hoarau Matthias <h.matthias@live.fr>
2021-01-20 09:18:20 +0400

    fix connection mongoDB creatIndex

--- app.js
+++ app.js
@@ -1,27 +1,31 @@
 require ("dotenv").config();
 const express = require ("express");
 const mongoose = require ("mongoose");
+const bodyParser = require ("body-parser");
 
 const userRoutes = require ("./routes/user");
 
-mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lzllw.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`,
-    {   useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log('Connexion à MongoDB réussie !'))
-    .catch(() => console.log('Connexion à MongoDB échouée !'));
-
 const app = express();
+
+mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lzllw.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`,
+    {   useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
+    .then(() => console.log('Connexion à MongoDB réussie !'))
+    .catch(() => console.log('Connexion à MongoDB échouée !')
+);
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
-  });
-
-app.use((req, res, next) => {
-    res.json({messge: "message bien recu"});
-    next;
 });
+
+app.use(bodyParser.json());
+
+// app.use((req, res, next) => {
+//     res.json({messge: "message bien recu"});
+//     next;
+// });
 
 
 app.use("/api/auth", userRoutes);

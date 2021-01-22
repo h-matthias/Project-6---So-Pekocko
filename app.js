22143f66bd935d513461c23007504fde7d500d46
Hoarau Matthias <h.matthias@live.fr>
2021-01-22 15:27:12 +0400

    fix path image

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use((req, res, next) => {
 
 app.use(bodyParser.json());
 
-app.use("/image", express.static(path.join(__dirname, "images")))
+app.use("/images", express.static(path.join(__dirname, "images")));
 
 
 app.use("/api/auth", userRoutes);

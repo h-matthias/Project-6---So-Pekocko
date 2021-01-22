43e3a4baa16e1f992f5a45533b1b42e97e7b2f6e
Hoarau Matthias <h.matthias@live.fr>
2021-01-22 15:32:14 +0400

    fix multer et ajout route deleteSauce et likedSauce

--- routes/sauce.js
+++ routes/sauce.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const multer = require("multer");
+const multer = require("../middleware/multer-config");
 const router = express.Router();
 const sauceCtrl = require("../controllers/sauce");
 const auth = require("../middleware/auth");
@@ -8,7 +8,7 @@ router.get("/", auth, sauceCtrl.getAllSauce);
 router.get("/:id", auth, sauceCtrl.getOneSauce);
 router.post("/", auth, multer, sauceCtrl.createSauce);
 router.put("/:id", auth, multer, sauceCtrl.modifySauce);
-// router.delete("/:id", auth, sauceCtrl.deleteSauce);
-// router.post("/:id/like", auth, sauceCtrl.likedSauce)
+router.delete("/:id", auth, sauceCtrl.deleteSauce);
+router.post("/:id/like", auth, sauceCtrl.likedSauce)
 
 module.exports = router;

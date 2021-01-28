bbfe68a5f6910b41102fa8da91b1337d79755949
Hoarau Matthias <h.matthias@live.fr>
2021-01-28 09:01:21 +0400

    up retour erreur deux email

--- models/User.js
+++ models/User.js
@@ -2,7 +2,7 @@ const mongoose = require ("mongoose");
 const uniqueValidator = require("mongoose-unique-validator");
 
 const userSchema = mongoose.Schema({
-    email: {type: String, unique: true, required: true },
+    email: {type: String, unique: true, required: true, unique: "Adresse Email déjà enregistrée." },
     password: {type: String, required: true},
 });
 

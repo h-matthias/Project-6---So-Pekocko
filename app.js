1895889c58b110cc5991f74ae783897b79a77c4a
Hoarau Matthias <h.matthias@live.fr>
2022-06-09 13:25:43 +0400

    supression dependance body-parser

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 require ("dotenv").config();
 const express = require ("express");
 const mongoose = require ("mongoose");
-const bodyParser = require ("body-parser");
 const path = require("path");
 
 const userRoutes = require ("./routes/user");
@@ -9,7 +8,7 @@ const sauceRoutes = require("./routes/sauce");
 
 const app = express();
 
-mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lzllw.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`,
+mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.pxvs0sw.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`,
     {   useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
     .then(() => console.log('Connexion à MongoDB réussie !'))
     .catch(() => console.log('Connexion à MongoDB échouée !')
@@ -22,7 +21,7 @@ app.use((req, res, next) => {
     next();
 });
 
-app.use(bodyParser.json());
+app.use(express.json());
 
 // permet d'afficher les images
 app.use("/images", express.static(path.join(__dirname, "images")));
